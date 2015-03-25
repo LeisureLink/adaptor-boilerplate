89a2475c7838d3ad397689592b25d89eca6cf021
Matt Fitzgerald <matt.fitzgerald@parivedasolutions.com>
2015-03-25 10:03:33 -0700

    comment changes

--- endpoints/controllers/stub_controller.js
+++ endpoints/controllers/stub_controller.js
@@ -10,12 +10,12 @@ $.stub = function(request, response){
     //TODO: get any params from the url
     //var rentalUnitId = request.params.id;
     async.waterfall([
-        //fetch data from LeisureLink API
+        //fetch data from external source
         function fetchData(callback) {
             async.parallel([
                 //TODO: Get all necessary information from LeisureLink API
-                //function (callback) {messenger.get(config.leisureLink.URLs.availability(rentalUnitId), callback)},
-                //function (callback) {messenger.get(config.leisureLink.URLs.baseRates(rentalUnitId), callback)}
+                //function (callback) {messenger.get(config.leisureLink.URLs.availability(rentalUnitId), null, callback)},
+                //function (callback) {messenger.get(config.leisureLink.URLs.baseRates(rentalUnitId), null, callback)}
             ],function(error, results){
                 if (error){
                     //TODO: define appropriate error messages. Remember to log identifying details, like unitId, confirmation#, etc.
@@ -57,8 +57,20 @@ $.stub = function(request, response){
         },
 
         //send validated data
-        function(transformedData, callback) {
-            messenger.sendResponse(response, 200, transformedData);
+        function handleValidatedDate(transformedData, callback) {
+            //TODO: send the transformed data to the appropriate party, whether that's sending it in a response or sending it to an API endpoint
+            //messenger.post(url, transformedData, null, function(error, results){
+            // if (error) {
+            //      callback(error);
+            //  }
+            //  else {
+            //      return callback(null, null);
+            //  }
+            // }
+            //
+            //   OR
+            //
+            //messenger.sendResponse(response, 200, transformedData);
             return callback(null);
         }
     ],
@@ -66,9 +78,11 @@ $.stub = function(request, response){
         function(error){
             if (error) {
                 logger.log('error', error.logMessage);
+                //TODO: define proper way to send failure to caller
+                //messenger.sendResponse({'error': error.responseMessage}, 400, transformedData);
             }
             else {
-                //TOOO: define appropriate log message. Remember to log identifying details like unitId, confirmation #, etc.
+                //TODO: define appropriate log message. Remember to log identifying details like unitId, confirmation #, etc.
                 logger.log('info'/*, 'Sent availability info for unit ' + rentalUnitId*/);
             }
         }
