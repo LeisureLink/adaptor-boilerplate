474d573ade58d86b9158b505839fda016f7edf72
Daniel Piorkowski <daniel.piorkowski@parivedasolutions.com>
2015-05-11 10:59:47 -0700

    Add logging to ping controller

--- endpoints/controllers/pingController.js
+++ endpoints/controllers/pingController.js
@@ -1,5 +1,9 @@
+var logger = rootRequire('logging/logger');
+
 var $ = {
     ping: function(request, response){
+        logger.log('info', 'Ping request received');
+
         response.status = 200;
         response.send('pong');
     }
