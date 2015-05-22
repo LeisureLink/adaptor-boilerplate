7df964541388d1ed637b46230bf490471d162751
Daniel Piorkowski <daniel.piorkowski@parivedasolutions.com>
2015-05-22 11:22:26 -0700

    Update routes to be Docker-environment compatible

--- routes.js
+++ routes.js
@@ -2,8 +2,8 @@ var pingController = rootRequire('endpoints/controllers/pingController');
 var stubController = rootRequire('endpoints/controllers/stub_controller');
 
 module.exports = function (app, router){
-    router.get('/ping', pingController.ping);
-    router.get('/stub', stubController.stub);
+    router.get('/(*/)?ping', pingController.ping);
+    router.get('/(*/)?stub', stubController.stub);
 
     app.use('/', router);
 };
