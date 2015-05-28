6c510d2657715f8a0fa6bd4484817d0038bcd5e5
Daniel Piorkowski <daniel.piorkowski@parivedasolutions.com>
2015-05-28 09:38:10 -0700

    Remove sensitive config values entirely from appDefaultEnv.js

--- settings/appDefaultEnv.js
+++ settings/appDefaultEnv.js
@@ -1,17 +1,14 @@
 // Loads all required environment variables from default values.
 // This is only for assisting developers and should not be included in the Docker image.
 
+// Note: DO NOT CHECK SENSITIVE VALUES INTO REPO - user names, passwords, API keys, etc.
+//       For the boilerplate adaptor, an example of what should be in Confluence can be found in
+//       exampleConfluence.txt in the repository root.
+
 if (!process.env.TEST_ADAPTOR_PORT) {
     process.env.TEST_ADAPTOR_PORT = '3016';
 }
 
 if (!process.env.TEST_LEISURE_LINK_API_BASE) {
     process.env.TEST_LEISURE_LINK_API_BASE = 'https://api-dev.leisurelink.com/v1/';
-}
-
-// TODO: Substitute value from Confluence
-// Note: For the boilerplate adaptor, an example of what should be in Confluence can be found in
-//       exampleConfluence.txt in the repository root. DO NOT CHECK SENSITIVE VALUES INTO REPO
-if (!process.env.TEST_LEISURE_LINK_API_KEY) {
-    //process.env.TEST_LEISURE_LINK_API_KEY = 'INSERT_API_KEY_HERE';
 }
