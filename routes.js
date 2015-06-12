a0d7d236c887affc7b6112e15fb0b52c38039d81
Daniel Piorkowski <daniel.piorkowski@parivedasolutions.com>
2015-06-12 14:23:20 -0700

    Update routes

--- routes.js
+++ routes.js
@@ -2,8 +2,9 @@ var pingController = rootRequire('endpoints/controllers/pingController');
 var stubController = rootRequire('endpoints/controllers/stub_controller');
 
 module.exports = function (app, router){
-    router.get('/(*/)?ping', pingController.ping);
-    router.get('/(*/)?stub', stubController.stub);
+    // TODO: Update application name
+    router.get('/adaptor-boilerplate/ping', pingController.ping);
+    router.get('/adaptor-boilerplate/stub', stubController.stub);
 
     app.use('/', router);
 };
