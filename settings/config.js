fc24a53153d08fae541b18dc9f0f1acc69e1e8f1
Daniel Piorkowski <daniel.piorkowski@parivedasolutions.com>
2015-06-16 14:54:23 -0700

    Fix config

--- settings/config.js
+++ settings/config.js
@@ -1,5 +1,4 @@
 var $ = {};
-var util = require('util');
 var deasync = require('deasync');
 var Configurator = require('env-configurator');
 var configSpec = require('./envConfigSpec');
@@ -14,37 +13,35 @@ function get(name) {
     return config.get('test', '#/' + name);
 }
 
-// TODO: Update populateConfig to assign config object properties using get()
-function populateConfig() {
-    $.port = process.env.PORT_NUMBER || get('adaptor_port');
+// TODO: Update config object to assign properties using get()
+$.port = process.env.PORT_NUMBER || get('adaptor_port');
 
-    $.leisureLink = {};
-    $.leisureLink.apiBase = get('leisure_link/api_base');
-    $.leisureLink.apiKey = get('leisure_link/api_key');
+$.leisureLink = {};
+$.leisureLink.apiBase = get('leisure_link/api_base');
+$.leisureLink.apiKey = get('leisure_link/api_key');
 
-    $.leisureLink.URLs = {
-        // Static API URLs
-        'quote'           : $.leisureLink.apiBase + 'quote',
-        'booking'         : $.leisureLink.apiBase + 'booking',
-        'cancellation'    : $.leisureLink.apiBase + 'cancel',
-        'rentalUnitsList' : $.leisureLink.apiBase + 'rentalUnits',
-        'updateLog'       : $.leisureLink.apiBase + 'updateLog',
+$.leisureLink.URLs = {
+    // Static API URLs
+    'quote'           : $.leisureLink.apiBase + 'quote',
+    'booking'         : $.leisureLink.apiBase + 'booking',
+    'cancellation'    : $.leisureLink.apiBase + 'cancel',
+    'rentalUnitsList' : $.leisureLink.apiBase + 'rentalUnits',
+    'updateLog'       : $.leisureLink.apiBase + 'updateLog',
 
-        // Dynamic API URLs
-        'rentalUnitDetail'            : function(unitId){return $.leisureLink.apiBase + 'rentalUnits/' + unitId;                        },
-        'rentalUnitAvailability'      : function(unitId){return $.leisureLink.apiBase + 'rentalUnits/' + unitId + '/availability';      },
-        'rentalUnitBaseRates'         : function(unitId){return $.leisureLink.apiBase + 'rentalUnits/' + unitId + '/baseRates';         },
-        'rentalUnitStayRestrictions'  : function(unitId){return $.leisureLink.apiBase + 'rentalUnits/' + unitId + '/stayRestrictions';  },
-        'rentalUnitCheckInInformation': function(unitId){return $.leisureLink.apiBase + 'rentalUnits/' + unitId + '/checkInInformation';},
-        'rentalUnitSpecials'          : function(unitId){return $.leisureLink.apiBase + 'rentalUnits/' + unitId + '/specials';          },
-        'specialAvailability'         : function(specialId){return $.leisureLink.apiBase + 'special/'  + specialId;                     }
-    };
+    // Dynamic API URLs
+    'rentalUnitDetail'            : function(unitId){return $.leisureLink.apiBase + 'rentalUnits/' + unitId;                        },
+    'rentalUnitAvailability'      : function(unitId){return $.leisureLink.apiBase + 'rentalUnits/' + unitId + '/availability';      },
+    'rentalUnitBaseRates'         : function(unitId){return $.leisureLink.apiBase + 'rentalUnits/' + unitId + '/baseRates';         },
+    'rentalUnitStayRestrictions'  : function(unitId){return $.leisureLink.apiBase + 'rentalUnits/' + unitId + '/stayRestrictions';  },
+    'rentalUnitCheckInInformation': function(unitId){return $.leisureLink.apiBase + 'rentalUnits/' + unitId + '/checkInInformation';},
+    'rentalUnitSpecials'          : function(unitId){return $.leisureLink.apiBase + 'rentalUnits/' + unitId + '/specials';          },
+    'specialAvailability'         : function(specialId){return $.leisureLink.apiBase + 'special/'  + specialId;                     }
+};
 
-    //TODO: Rename with the appropriate integrator (ResortZilla, Outrigger, etc.)
-    $.INTEGRATOR = {};
-    $.INTEGRATOR.URLs = {
-        //TODO: Add appropriate integrator URLs
-    };
-}
+//TODO: Rename with the appropriate integrator (ResortZilla, Outrigger, etc.)
+$.INTEGRATOR = {};
+$.INTEGRATOR.URLs = {
+    //TODO: Add appropriate integrator URLs
+};
 
 module.exports = $;
